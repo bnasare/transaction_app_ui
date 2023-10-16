6fdaaf8eb622ae1bb707510a4e108c498afbb26d
bnasare <124492150+bnasare@users.noreply.github.com>
2023-10-16 23:55:26 +0000

    bottom app bar updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+import '../widgets/bottom_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home';
-  const HomeScreen({super.key});
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  final int selectedIndex = 0; // Define the selectedIndex here
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +18,15 @@ class HomeScreen extends StatelessWidget {
           'Shoop',
           style: Theme.of(context).textTheme.titleLarge,
         ),
+      ),
+      bottomNavigationBar: CustomTab(
+        icon: IconlyLight.home,
+        label: 'Home',
+        isSelected: selectedIndex == 0,
+        color: Theme.of(context).colorScheme,
+        onTap: () {
+          // Add your logic for the Home tab here
+        },
       ),
     );
   }
--- lib/widgets/bottom_appbar.dart
+++ lib/widgets/bottom_appbar.dart
@@ -1,147 +1,38 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_iconly/flutter_iconly.dart';
 
-class CustomBottomAppBar extends StatelessWidget {
-  final int selectedIndex;
+class CustomTab extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final bool isSelected;
+  final ColorScheme color;
+  final Function() onTap;
 
-  const CustomBottomAppBar({
-    Key? key,
-    required this.selectedIndex,
-  }) : super(key: key);
+  const CustomTab({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.isSelected,
+    required this.color,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    var color = Theme.of(context).colorScheme;
-
-    return BottomAppBar(
-      shadowColor: Colors.transparent,
-      color: color.surface,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          //! Home Tab
-          InkWell(
-            onTap: () {},
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  IconlyLight.home,
-                  color: selectedIndex == 0
-                      ? Theme.of(context).colorScheme.secondary
-                      : Theme.of(context).colorScheme.tertiary,
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  'Home',
-                  style: TextStyle(
-                    fontWeight: selectedIndex == 0
-                        ? FontWeight.w600
-                        : FontWeight.normal,
-                    color:
-                        selectedIndex == 0 ? color.onTertiary : color.tertiary,
-                  ),
-                )
-              ],
-            ),
+          Icon(
+            icon,
+            color: isSelected ? color.secondary : color.tertiary,
           ),
-
-          //! Compass Tab
-          InkWell(
-            onTap: () {},
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  CupertinoIcons.cube,
-                  color: selectedIndex == 1
-                      ? Theme.of(context).colorScheme.secondary
-                      : Theme.of(context).colorScheme.tertiary,
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  'Packet',
-                  style: TextStyle(
-                    fontWeight: selectedIndex == 1
-                        ? FontWeight.w600
-                        : FontWeight.normal,
-                    color:
-                        selectedIndex == 1 ? color.onTertiary : color.tertiary,
-                  ),
-                )
-              ],
-            ),
-          ),
-
-          //! Add Button
-          Padding(
-            padding: const EdgeInsets.only(bottom: 12.0),
-            child: SizedBox(
-              height: 50,
-              width: 50,
-              child: FloatingActionButton(
-                backgroundColor: color.secondary,
-                onPressed: () {},
-                child: const Icon(
-                  CupertinoIcons.add,
-                  size: 28,
-                ),
-              ),
-            ),
-          ),
-
-          //! Bookmark Tab
-          InkWell(
-            onTap: () {},
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  IconlyLight.discount,
-                  color: selectedIndex == 2
-                      ? Theme.of(context).colorScheme.secondary
-                      : Theme.of(context).colorScheme.tertiary,
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  'Home',
-                  style: TextStyle(
-                    fontWeight: selectedIndex == 2
-                        ? FontWeight.w600
-                        : FontWeight.normal,
-                    color:
-                        selectedIndex == 2 ? color.onTertiary : color.tertiary,
-                  ),
-                )
-              ],
-            ),
-          ),
-
-          //! Profile Tab
-          InkWell(
-            onTap: () {},
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  IconlyLight.profile,
-                  color: selectedIndex == 3
-                      ? Theme.of(context).colorScheme.secondary
-                      : Theme.of(context).colorScheme.tertiary,
-                ),
-                const SizedBox(height: 3),
-                Text(
-                  'Home',
-                  style: TextStyle(
-                    fontWeight: selectedIndex == 3
-                        ? FontWeight.w600
-                        : FontWeight.normal,
-                    color:
-                        selectedIndex == 3 ? color.onTertiary : color.tertiary,
-                  ),
-                )
-              ],
+          const SizedBox(height: 5),
+          Text(
+            label,
+            style: TextStyle(
+              fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+              color: isSelected ? color.onTertiary : color.tertiary,
             ),
           ),
         ],
