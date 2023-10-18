add4f4649a8f77cc196443dffd98b46f21e6c7f2
bnasare <124492150+bnasare@users.noreply.github.com>
2023-10-18 21:01:29 +0000

    overflow errors fixed

--- lib/widgets/balance_card.dart
+++ lib/widgets/balance_card.dart
@@ -26,36 +26,36 @@ class UserBalanceCard extends StatelessWidget {
               left: 3,
               bottom: 15,
             ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Flexible(
-                  flex: 2,
-                  child: ListTile(
-                    title: Text(
-                      'Your balance',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: color.onSecondary,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(top: 9),
-                      child: Text(
-                        '\$1,250.12',
-                        style: textTheme.bodyLarge?.copyWith(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 12.0, right: 40, left: 20),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Your balance',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: color.onSecondary,
                           fontWeight: FontWeight.w600,
-                          fontSize: 24,
                         ),
                       ),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 9),
+                        child: Text(
+                          '\$1,250.12',
+                          style: textTheme.bodyLarge?.copyWith(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                const Spacer(),
-                Padding(
-                  padding: const EdgeInsets.only(top: 12.0),
-                  child: RotatedBox(
+                  const Spacer(),
+                  RotatedBox(
                     quarterTurns: -1,
                     child: SizedBox(
                       width: 55.0,
@@ -67,34 +67,32 @@ class UserBalanceCard extends StatelessWidget {
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 20,
-                    right: 40,
-                    top: 12,
-                  ),
-                  child: Column(
-                    children: [
-                      Icon(
-                        Icons.wallet,
-                        size: 28,
-                        color: color.primary,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 7.0),
-                        child: Text(
-                          'Top-up',
-                          style: TextStyle(
-                            color: color.onSecondary,
-                            fontWeight: FontWeight.w600,
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                    ),
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.wallet,
+                          size: 28,
+                          color: color.primary,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 7.0),
+                          child: Text(
+                            'Top-up',
+                            style: TextStyle(
+                              color: color.onSecondary,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           const CustomDivider(indent: 20, endIndent: 20),
