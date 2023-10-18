b3b0ea4559e9c4222e36bd4b2f10be25e34b21c8
bnasare <124492150+bnasare@users.noreply.github.com>
2023-10-18 17:28:16 +0000

    Service icons row UI update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -13,11 +15,10 @@ void main() {
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
     GoogleFonts.config.allowRuntimeFetching = false;
     runApp(
-      // DevicePreview(
-      //   enabled: !kReleaseMode,
-      // builder: (context) =>
-      const MyApp(),
-      // ),
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => const MyApp(),
+      ),
     );
   });
 }
--- lib/widgets/service_icons_row.dart
+++ lib/widgets/service_icons_row.dart
@@ -6,30 +6,33 @@ class ServiceIconsRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        ServiceOptionButton(
-          iconData: Icons.local_shipping_outlined,
-          title: 'SV-Truck',
-          onPressed: () {},
-        ),
-        ServiceOptionButton(
-          iconData: CupertinoIcons.airplane,
-          title: 'SV-Air',
-          onPressed: () {},
-        ),
-        ServiceOptionButton(
-          iconData: Icons.directions_boat_filled_outlined,
-          title: 'SV-Sea',
-          onPressed: () {},
-        ),
-        ServiceOptionButton(
-          iconData: Icons.headphones_outlined,
-          title: 'SV-Help',
-          onPressed: () {},
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 23, right: 23, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ServiceOptionButton(
+            iconData: Icons.local_shipping_outlined,
+            title: 'SV-Truck',
+            onPressed: () {},
+          ),
+          ServiceOptionButton(
+            iconData: CupertinoIcons.airplane,
+            title: 'SV-Air',
+            onPressed: () {},
+          ),
+          ServiceOptionButton(
+            iconData: Icons.directions_boat_filled_outlined,
+            title: 'SV-Sea',
+            onPressed: () {},
+          ),
+          ServiceOptionButton(
+            iconData: Icons.headphones_outlined,
+            title: 'SV-Help',
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 }
@@ -51,46 +54,38 @@ class ServiceOptionButton extends StatelessWidget {
     final color = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
 
-    return Padding(
-      padding: const EdgeInsets.only(left: 23, right: 23, top: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            children: [
-              Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      color.onPrimary.withOpacity(0.05),
-                      color.onPrimary.withOpacity(0.5),
-                    ],
-                  ),
-                ),
-                child: Center(
-                  child: IconButton(
-                    onPressed: onPressed,
-                    icon: Icon(iconData),
-                    color: color.onPrimary,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15.0),
-                child: Text(
-                  title,
-                  style: textTheme.titleMedium,
-                ),
-              ),
-            ],
+    return Column(
+      children: [
+        Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                color.onPrimary.withOpacity(0.05),
+                color.onPrimary.withOpacity(0.5),
+              ],
+            ),
           ),
-        ],
-      ),
+          child: Center(
+            child: IconButton(
+              onPressed: onPressed,
+              icon: Icon(iconData),
+              color: color.onPrimary,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 15.0),
+          child: Text(
+            title,
+            style: textTheme.titleMedium,
+          ),
+        ),
+      ],
     );
   }
 }
