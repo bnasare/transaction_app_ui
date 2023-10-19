5533d668c666b7476ec2251111d0af3c5a20bdaa
bnasare <124492150+bnasare@users.noreply.github.com>
2023-10-19 05:57:55 +0000

    images updated

--- lib/consts/insight_item.dart
+++ lib/consts/insight_item.dart
@@ -1,9 +1,9 @@
 import '../models/insight_item_model.dart';
 
 List<InsightItem> insightItems = [
-  InsightItem(imagePath: 'assets/images/man.jpg'),
   InsightItem(imagePath: 'assets/images/woman.jpg'),
   InsightItem(imagePath: 'assets/images/man.jpg'),
-  InsightItem(imagePath: 'assets/images/woman.jpg'),
-  InsightItem(imagePath: 'assets/images/man.jpg'),
+  InsightItem(imagePath: 'assets/images/human.jpg'),
+  InsightItem(imagePath: 'assets/images/person.jpg'),
+  InsightItem(imagePath: 'assets/images/girl.jpg'),
 ];
